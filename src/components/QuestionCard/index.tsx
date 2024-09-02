3e2e44dd5e98f63d36640f96a6111db961dff7a9
openmpy <suhwan@kakao.com>
2024-09-02 16:44:46 +0900

    feat: Update QuestionCard component with Chakra UI styling and progress display

--- src/components/QuestionCard/index.tsx
+++ src/components/QuestionCard/index.tsx
@@ -1,5 +1,61 @@
-const QuestionCard = () => {
-  return <div>QuestionCard</div>;
+import { Box, Flex, Heading, Spacer } from "@chakra-ui/react";
+import React from "react";
+import AppButton from "../AppButton";
+
+interface QuestionCardProps {
+  question: string;
+  category: string;
+  totalQuestions?: number;
+  questionNumber?: number;
+  checkAnswer: (e: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+const QuestionCard: React.FC<QuestionCardProps> = ({
+  question,
+  checkAnswer,
+  category,
+  totalQuestions,
+  questionNumber,
+}) => {
+  return (
+    <>
+      <Box bg="white" w="100%">
+        <Box mb={6} fontSize="md" fontWeight="bold" textTransform="uppercase">
+          Your progresss: {questionNumber} / {totalQuestions}
+        </Box>
+
+        <Box fontSize="sm" mb={1}>
+          {category}
+        </Box>
+
+        <Heading as="h1" size="lg">
+          <p dangerouslySetInnerHTML={{ __html: question }}></p>
+        </Heading>
+
+        <Flex direction="column">
+          <Box w="100%" mt={4}>
+            <AppButton
+              colorScheme="purple"
+              variant="outline"
+              onClick={checkAnswer}
+              value="True"
+              width="full"
+            />
+          </Box>
+          <Spacer />
+          <Box>
+            <AppButton
+              colorScheme="purple"
+              variant="outline"
+              onClick={checkAnswer}
+              value="False"
+              width="full"
+            />
+          </Box>
+        </Flex>
+      </Box>
+    </>
+  );
 };
 
 export default QuestionCard;
