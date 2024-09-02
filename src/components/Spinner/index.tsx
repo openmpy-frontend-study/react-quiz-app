429ceef8f1df5aa6e0d42d0482603f56987a6004
openmpy <suhwan@kakao.com>
2024-09-02 16:48:34 +0900

    feat: Refactor Spinner component to use Chakra UI

--- src/components/Spinner/index.tsx
+++ src/components/Spinner/index.tsx
@@ -1,5 +1,30 @@
-const Spinner = () => {
-  return <div>Spinner</div>;
+import { Spinner } from "@chakra-ui/react";
+import React from "react";
+
+interface SpinnerProps {
+  thickness: string;
+  speed: string;
+  color: string;
+  emptyColor: string;
+  size: string;
+}
+
+const AppSpinner: React.FC<SpinnerProps> = ({
+  thickness,
+  speed,
+  color,
+  emptyColor,
+  size,
+}) => {
+  return (
+    <Spinner
+      thickness={thickness}
+      speed={speed}
+      color={color}
+      emptyColor={emptyColor}
+      size={size}
+    />
+  );
 };
 
 export default Spinner;
