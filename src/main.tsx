3de12183c84c4aa1038ab163a0d3b16e6f2cc146
openmpy <suhwan@kakao.com>
2024-09-02 16:24:29 +0900

    chore: Update npm dependencies and add Chakra UI library

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { ChakraProvider } from "@chakra-ui/react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <ChakraProvider>
     <App />
-  </StrictMode>,
-)
+  </ChakraProvider>
+);
